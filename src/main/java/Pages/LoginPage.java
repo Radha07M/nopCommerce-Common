bf5e4644ffed1cf3b43ba405b775893dfb387f53
Radha07M <radha07mane@gmail.com>
2023-04-17 14:31:45 +0530

    Added JUNIT library and assertion
    Added JUNIT library and assertion

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,9 +1,12 @@
 package Pages;
 
+import org.junit.*;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
+
 
 import PageObjects.*;
 public class LoginPage extends LoginObjects{
@@ -17,6 +20,7 @@ public class LoginPage extends LoginObjects{
 		PageFactory.initElements(driver, this);
 		driver.get("https://admin-demo.nopcommerce.com/login");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	public void enterEmailAndPassword(String email, String passwd) {
 		username.clear();
@@ -30,11 +34,12 @@ public class LoginPage extends LoginObjects{
 	}
 	
 	public void clickOnLogoutLink() {
-		logOut.click();
+		logOut.click();	
 	}
 	
 	public void VerifyPageTitle(String title) {
-		
+		String pageTitle = driver.getTitle();
+		Assert.assertTrue("Page Title is matching", pageTitle.equals(title));
 	}
 	
 	public void closeBrowser()
