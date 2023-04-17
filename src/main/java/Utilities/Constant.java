42b615d21ffddd27b2ae6c8344567b44efff5b6c
Radha07M <radha07mane@gmail.com>
2023-04-17 15:48:53 +0530

    updated constant
    updated constant

--- src/main/java/Utilities/Constant.java
+++ src/main/java/Utilities/Constant.java
@@ -3,4 +3,6 @@ package Utilities;
 public interface Constant {
 	String APPDATA_PATH = "/src/main/resources/Properties/AppData.properties";
 	String TESTDATA_PATH = "/src/main/resources/Properties/TestData.properties";
+	String ERRORMESSAGE_PATH="/src/main/resources/Properties/ErrorMessage.properties";
+	String SUCCESSMESSAGE_PATH="/src/main/resources/Properties/SuccessMessage.properties";
 }
